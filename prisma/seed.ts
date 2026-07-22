986b3306375aa0a2207cf0dca90c67bb28a54066
A. K <74007089+soggy-mushroom@users.noreply.github.com>
2026-07-22 21:43:50 +0100

    Seed the database with a sample tasklist

---
+++ prisma/seed.ts
@@ -0,0 +1,28 @@
+import { PrismaPg } from '@prisma/adapter-pg'
+import { PrismaClient } from "../src/generated/prisma/client";
+
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL })
+const prisma = new PrismaClient({ adapter });
+
+
+async function main() {
+  await prisma.taskList.create({
+    data: {
+      name: "tasklist1",
+      tasks: {
+        create: [
+          {
+            title: "do the laundry",
+          },
+          {
+            title: "cook dinner",
+          },
+        ],
+      },
+    },
+  });
+}
+
+main().then(() => {
+  console.log("Data seeded...");
+});
