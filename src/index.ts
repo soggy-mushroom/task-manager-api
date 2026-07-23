edece44d48643953c4c1e461e8a992520974516d
A. K <74007089+soggy-mushroom@users.noreply.github.com>
2026-07-23 12:05:03 +0100

    Create a graphql server with yoga

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
-console.log("Hello world");
+import { createYoga } from "graphql-yoga";
+import { createServer } from "http";
+
+const yoga = createYoga({});
+const server = createServer(yoga);
+
+server.listen(4000, "127.0.0.1");
