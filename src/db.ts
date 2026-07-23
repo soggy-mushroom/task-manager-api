a52ab77102c629b4ed5894cddbdb64c136021cda
A. K <74007089+soggy-mushroom@users.noreply.github.com>
2026-07-23 20:04:12 +0100

    add adapter to the reusable instance of prisma client

--- src/db.ts
+++ src/db.ts
@@ -1,2 +1,6 @@
 import { PrismaClient } from "./generated/prisma/client";
-export const prisma = new PrismaClient();
+import { PrismaPg } from '@prisma/adapter-pg';
+
+
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL })
+export const prisma = new PrismaClient({ adapter });
