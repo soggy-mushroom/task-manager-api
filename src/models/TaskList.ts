0b25d2c512402f41b12a7005431a63cab85453ef
A. K <74007089+soggy-mushroom@users.noreply.github.com>
2026-07-24 14:03:14 +0100

    Small formatting fixes and remove unused args

--- src/models/TaskList.ts
+++ src/models/TaskList.ts
@@ -2,20 +2,20 @@ import { builder } from "../builder";
 import { prisma } from "../db";
 
 builder.prismaObject("TaskList", {
-  fields: t => ({
+  fields: (t) => ({
     id: t.exposeID("id"),
     name: t.exposeString("name"),
     createdAt: t.expose("createdAt", {
       type: "Date",
     }),
-    tasks: t.relation("tasks")
-})
+    tasks: t.relation("tasks"),
+  }),
 });
 
 builder.queryField("taskLists", (t) =>
   t.prismaField({
     type: ["TaskList"],
-    resolve: async (query, root, args, ctx, info) => {
+    resolve: async (query) => {
       return prisma.taskList.findMany(query);
     },
   })
